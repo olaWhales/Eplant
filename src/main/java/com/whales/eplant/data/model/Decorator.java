23d04cdceb226a9966dff8a5dc0ffdf60b6d276d
olawhales <ajaditaoreed@gmail.com>
2025-04-24 19:31:51 +0100

    Addition of makeUp,Decorator,Photographer and Caterer

---
+++ src/main/java/com/whales/eplant/data/model/Decorator.java
@@ -0,0 +1,24 @@
+package com.whales.eplant.data.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Decorator {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+}
---
+++ src/main/java/com/whales/eplant/data/model/MakeUp.java
@@ -0,0 +1,22 @@
+package com.whales.eplant.data.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MakeUp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+}
