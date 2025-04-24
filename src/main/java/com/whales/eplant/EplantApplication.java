e54d9b0d48f1ae12a6da910b80519b61fc1426ab
olawhales <ajaditaoreed@gmail.com>
2025-04-24 19:10:11 +0100

    Eplant project creation

---
+++ src/main/java/com/whales/eplant/EplantApplication.java
@@ -0,0 +1,13 @@
+package com.whales.eplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EplantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EplantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whales/eplant/EplantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whales.eplant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EplantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
