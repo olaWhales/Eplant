ea9d9f6da5d9c31e615dee9e4afe562529387df8
olawhales <ajaditaoreed@gmail.com>
2025-04-25 10:30:26 +0100

    adding of request and response packages

--- src/main/java/com/whales/eplant/email/EmailService.java
+++ src/main/java/com/whales/eplant/email/EmailService.java
@@ -1,12 +1,16 @@
 package com.whales.eplant.email;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class EmailService {
-    private final JavaMailSender mailSender;
+    private final JavaMailSender mailSender ;
 
     public EmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
