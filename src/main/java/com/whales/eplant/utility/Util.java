709835a2a34de0adb483ce93f974a30f0a9e9c79
olawhales <ajaditaoreed@gmail.com>
2025-04-25 10:52:28 +0100

    Addition of Utility class

---
+++ src/main/java/com/whales/eplant/utility/Util.java
@@ -0,0 +1,8 @@
+package com.whales.eplant.utility;
+
+public class Util {
+
+    public static final String USER_ALREADY_EXISTS = "User already exists";
+    public static final String VENDOR_ALREADY_REGISTER = "User already exists";
+
+}
